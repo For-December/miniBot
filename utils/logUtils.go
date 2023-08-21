b994f7ca22a2c59db39140229c9791b2d8770d7b
forDecember <1921567337@qq.com>
2023-08-22 05:13:01 +0800

    update: 重构自定义日志 优化代码

--- utils/logUtils.go
+++ utils/logUtils.go
@@ -27,36 +27,40 @@ func overridePrintln(l *log.Logger, v ...any) {
 	}
 }
 
+func colorPrint(color string, msg string, v ...any) {
+	myLogger.SetPrefix(color + msg + colorReset)
+	overridePrintln(myLogger, color+fmt.Sprint(v...)+colorReset)
+	// 上一行参数传v表示整体当成数组传入，参数传v... 表示多个参数分别传入
+}
+
+func colorPrintf(format string, color string, msg string, v ...any) {
+	myLogger.SetPrefix(color + msg + colorReset)
+	overridePrintln(myLogger, color+fmt.Sprintf(format, v...)+colorReset)
+}
+
 func Info(v ...any) {
-	myLogger.SetPrefix(colorGreen + "[Info]" + colorReset)
-	overridePrintln(myLogger, colorGreen+fmt.Sprint(v...)+colorReset)
+	colorPrint(colorGreen, "[Info]", v...)
 }
 func Error(v ...any) {
-	myLogger.SetPrefix(colorRed + "[Error]" + colorReset)
-	overridePrintln(myLogger, colorRed+fmt.Sprint(v...)+colorReset)
+	colorPrint(colorRed, "[Error]", v...)
 	os.Exit(1)
 }
 func Warning(v ...any) {
-	myLogger.SetPrefix(colorYellow + "[Warning]" + colorReset)
-	overridePrintln(myLogger, colorYellow+fmt.Sprint(v...)+colorReset)
-	// 上一行参数传v表示整体当成数组传入，参数传v... 表示多个参数分别传入
+	colorPrint(colorYellow, "[Warning]", v...)
 }
 
 // InfoF 带格式化的信息日志
 func InfoF(format string, v ...any) {
-	myLogger.SetPrefix(colorGreen + "[Info]" + colorReset)
-	overridePrintln(myLogger, colorGreen+fmt.Sprintf(format, v...)+colorReset)
+	colorPrintf(format, colorGreen, "[Info]", v...)
 }
 
 // ErrorF 带格式化的错误日志
 func ErrorF(format string, v ...any) {
-	myLogger.SetPrefix(colorRed + "[Error]" + colorReset)
-	overridePrintln(myLogger, colorRed+fmt.Sprintf(format, v...)+colorReset)
+	colorPrintf(format, colorGreen, "[Error]", v...)
 	os.Exit(1)
 }
 
 // WarningF 带格式化的警告日志
 func WarningF(format string, v ...any) {
-	myLogger.SetPrefix(colorYellow + "[Warning]" + colorReset)
-	overridePrintln(myLogger, colorYellow+fmt.Sprintf(format, v...)+colorReset)
+	colorPrintf(format, colorGreen, "[Warning]", v...)
 }
